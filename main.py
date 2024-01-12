65878cb844bcb83e58120a108cea079ac5a127fd
Rohith Mandavilli <Rohith.Mandavilli.22@Dartmouth.edu>
2024-01-11 21:42:23 -0600

    test

--- main.py
+++ main.py
@@ -3,28 +3,77 @@ import time
 import asyncio
 import json
 
-
-def analyze_10k(company_name):
-    pass
-
-
-def synthesize_market_news(company_name):
-    pass
+import requests
+import pandas as pd
 
 
-def analyze_competitors(company_name):
-    pass
+class Screener:
+    
+    
+    def __init__(self):
+        self.ticker_to_cik = {}
+
+        # create request header
+        headers = {'User-Agent': "email@address.com"}
+
+        # get all companies data
+        company_tickers = requests.get(
+            "https://www.sec.gov/files/company_tickers.json",
+            headers=headers
+        )
+        # review response / keys
+        company_ticker_data = company_tickers.json()
+        for key in company_ticker_data.keys():
+            company = company_ticker_data[key]
+            self.ticker_to_cik[company['ticker']] = company['cik_str']
+        
+        print("...retrieved company data...")
+
+
+    def analyze_10k(company_ticker):
+        """
+        get SEC filings from EDGAR, start with 10k
+        """
+        
+        # get company specific filing metadata
+        # filingMetadata = requests.get(
+        #     f'https://data.sec.gov/submissions/CIK{cik}.json',
+        #     headers=headers
+        # )
+
+        # # review json 
+        # print(filingMetadata.json().keys())
+        # filingMetadata.json()['filings']
+        # filingMetadata.json()['filings'].keys()
+        # filingMetadata.json()['filings']['recent']
+        # filingMetadata.json()['filings']['recent'].keys()
+
+        # # dictionary to dataframe
+        # allForms = pd.DataFrame.from_dict(
+        #     filingMetadata.json()['filings']['recent']
+        # )
+
+
+
+    def synthesize_market_news(company_name):
+        pass
+
+
+    def analyze_competitors(company_name):
+        pass
 
 
 async def main():
     while True:
-        user_input = input("Enter company name: ")
+        user_input = input("Enter company ticker: ")
         if user_input.lower() == 'exit':
             break
+    
+        screener = Screener()
 
-        filings_analysis = analyze_10k(user_input)
-        market_analysis = synthesize_market_news(user_input)
-        competitor_analysis = analyze_competitors(user_input)
+        filings_analysis = screener.analyze_10k(user_input)
+        market_analysis = screener.synthesize_market_news(user_input)
+        competitor_analysis = screener.analyze_competitors(user_input)
 
         print('Company Analysis:')
 
