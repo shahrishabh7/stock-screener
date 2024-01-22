c256a564e28f6ebdbfebcef56ff1ea0b804bf4cc
Rishabh Shah <shahrishabh7@gmail.com>
2024-01-22 15:05:47 -0600

    remove anthropic;

--- beautifulsoup.py
+++ beautifulsoup.py
@@ -3,7 +3,6 @@ import os
 from typing import List
 import requests
 from bs4 import BeautifulSoup
-from anthropicService import ClaudeService, HumanAssistantPrompt
 from dotenv import load_dotenv
 import pdfkit
 
@@ -20,10 +19,8 @@ class BeautifulSoupService:
         assert page.status_code, 200
         self.page_content = page.content
         self.html = BeautifulSoup(self.page_content, "html.parser")
-        self.claude = ClaudeService(api_key=ANTHROPIC_API_KEY)
 
         try:
-            # need to run brew install Caskroom/cask/wkhtmltopdf to use successfully
             pdfkit.from_url(url, self.pdf_path)
             print(f"PDF generated and saved at {self.pdf_path}")
         except Exception as e:
@@ -31,27 +28,11 @@ class BeautifulSoupService:
 
     async def get_page_content(self) -> str:
         pgraphs = await self.get_article_from_html()
-
-        prompt = HumanAssistantPrompt(
-            human_prompt=f"Below is a numbered list of the text in all the <p> and <li> tags on a web page: {pgraphs} Within this list, some lines may not be relevant to the primary content of the page (e.g. footer text, advertisements, etc.). Please identify the range of line numbers that correspond to the main article's content (i.e. article's paragraphs). Your response should only mention the range of line numbers, for example: 'lines 5-25'.",
-            assistant_prompt="Given the extracted text, the main content's line numbers are:",
-        )
-
-        line_nums = await self.claude.completion(
-            prompt=prompt,
-            max_tokens_to_sample=500,
-            temperature=0,
-        )
-
-        if len(line_nums) == 0:
-            return ""
-
-        content = self.extract_content_from_line_nums(pgraphs, line_nums)
-        return "\n".join(content)
+        return pgraphs
 
     async def get_text_from_sec_html(self) -> str:
         elements = []
-        
+
         for span in self.html.find_all("span"):
             if span.find_parent("table"):
                 continue
@@ -59,7 +40,7 @@ class BeautifulSoupService:
 
         if not elements:
             return "No <span> tags found on page"
-        
+
         formatted_elements = []
         for i, element in enumerate(elements):
             # remove extra whitespaces \s+ matches multiple spaces in a row
@@ -68,16 +49,16 @@ class BeautifulSoupService:
             formatted_elements.append(f"{prefix}{text}")
 
         return "\n".join(formatted_elements)
-    
+
     async def get_tables_from_sec_html(self) -> str:
         table_elements = []
 
         for table in self.html.find_all("table"):
             table_elements.append(table)
-        
+
         if not table_elements:
             return "No <table> tags found on page"
-    
+
         formatted_elements = []
         for i, element in enumerate(table_elements):
             # remove extra whitespaces \s+ matches multiple spaces in a row
@@ -109,22 +90,3 @@ class BeautifulSoupService:
             formatted_elements.append(f"{prefix}{text}")
 
         return "\n".join(formatted_elements)
-
-    @staticmethod
-    def extract_content_from_line_nums(self, pgraphs: str, line_nums: str) -> List[str]:
-        pgraph_elements = pgraphs.split("\n")
-        content = []
-        for line_num in line_nums.split(","):
-            if "-" in line_num:
-                start, end = self.extract_initial_line_numbers(line_num)
-                if start and end:
-                    for i in range(start, min(end + 1, len(pgraph_elements) + 1)):
-                        text = ".".join(
-                            pgraph_elements[i - 1].split(".")[1:]).strip()
-                        content.append(text)
-            elif line_num.isdigit():
-                text = ".".join(
-                    pgraph_elements[int(line_num) - 1].split(".")[1:]
-                ).strip()
-                content.append(text)
-        return content
